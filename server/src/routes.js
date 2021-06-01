64d06005d9aef8a725fce710f2ed6e1c8c3b3feb
Rasmus Dyrhave Wilson Nissen <rasmusdyrhavenissen@gmail.com>
2021-06-01 17:49:50 +0200

    Added the DB to routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,14 +1,50 @@
-module.exports = () => {
+module.exports = (postDB) => {
   const express = require("express");
   const router = express.Router();
 
   /**** Routes ****/
-  router.get('/disccamp', async (req, res) => {
-    res.json({msg: "Welcome!"});
+  router.get('/', async (req, res) => {
+    const posts = await postDB.getPosts();
+    res.json(posts);
   });
 
-  router.get('/disccamp/:name', async (req, res) => {
-    res.json({msg: `Hello, ${req.params.name}`});
+  router.get('/', async (req, res) => {
+    const post = req.body;
+
+    if (post && post.title && post.description) {
+      const savedPost = await postDB.createPost(post.title, post.description);
+      res.json(savedPost);
+    } else {
+      res.status(400).send("Missing title and/or description");
+    }
+  });
+
+  router.get('/:id', async (req, res) => {
+    const post = await postDB.getPost(req.params.id);
+    res.json(post);
+  });
+
+  router.post('/:id/comments', async (req, res) => {
+    const id = req.params.id;
+    const comment = req.body;
+
+    if (id && comment && comment.comment) {
+      const newComment = await postDB.createComment(id, comment.comment);
+      res.json(newComment);
+    } else {
+      res.status(400).send("Missing id or comment");
+    }    
+  });
+
+  router.post('/:id/votes/:commentId', async (req, res) => {
+    const commentId = req.params.commentId;
+
+    if (commentId) {
+      await postDB.vote(commentId, "up");
+      res.status(200).send("Ok");
+    } else {
+      res.status(400).send("Missing commentId");
+    }
   });
 
   return router;
